0da1ffc1540ab5bb5c993dddc6ddf102b6b6cc2c
Ji Myoung Ha <NoPlayer40600@gmail.com>
2022-12-12 14:45:03 +0900

    빌드 및 실행 기본 설정

---
+++ src/launch/Client.java
@@ -0,0 +1,9 @@
+package launch;
+
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println("This is Client.");
+    }
+
+}
---
+++ src/launch/Server.java
@@ -0,0 +1,9 @@
+package launch;
+
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("This is Server.");
+    }
+
+}
